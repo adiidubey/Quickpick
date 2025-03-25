f53a91061577bcfe9b1d2632dc4db8f235799d6a
adiidubey <dubeyaditya397@gmail.com>
2025-03-25 11:52:24 +0530

    commit

--- server/server.js
+++ server/server.js
@@ -25,7 +25,7 @@ const PORT = process.env.PORT;
 
 app.use(
 	cors({
-		origin: "http://localhost:5173",
+		origin: "*",
 		methods: ["GET", "POST", "DELETE", "PUT"],
 		allowedHeaders: [
 			"Content-Type",
